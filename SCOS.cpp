d43fe7d08333041e6d9f6a1691c3ddcc8087bbe0
giobkboy <giobkboy@gmail.com>
2013-01-27 22:50:18 -0500

    added metods

--- SCOS.cpp
+++ SCOS.cpp
@@ -8,17 +8,263 @@ using namespace std;
  V: 2.7.8
  */
 
+int add() {
+	long x;
+	long y;
+	long sum;
+            
+	cout<<"addition"<<endl;
+            
+	cin >> x;
+	cout <<"+";
+	cin >> y;
+            
+	sum = (x + y);
+
+	cout << x <<" + "<< y <<" = "<< sum <<endl;
+	return 0;
+}
+
+int subtract() {
+	long x;
+	long y;
+	long sum;
+    
+	cout<<"subtraction"<<endl;
+    
+	cin >> x;
+	cout <<"-";
+	cin >> y;
+    
+	sum = (x - y);
+    
+	cout << x <<" - "<< y <<" = "<< sum <<endl;
+	return 0;
+}
+int mul() {
+
+	 
+	long x;
+	long y;
+	long sum;
+    
+	cout<<"Multiplication"<<endl;
+    
+	cin >> x;
+	cout <<"*";
+	cin >> y;
+    
+	sum = (x * y);
+    
+	cout << x <<" * "<< y <<" = "<< sum <<endl;
+	return 0;
+}
+int dev(){
+	long x;
+	long y;
+	long sum;
+    
+	cout<<"division"<<endl;
+    
+	cin >> x;
+	cout <<"/";
+	cin >> y;
+    
+	sum = (x / y);
+    
+	cout << x <<" / "<< y <<" = "<< sum <<endl;
+	return 0;
+}
+int exp(){
+
+	string eop;  //to make shue that this is set to nothing
+	eop = " ";
+    
+	cout<< "-------------------------------------------------"<<endl;
+    
+	while(eop != "exit"){
+        
+		cout << ">: ";
+		cin >> eop;
+        
+		if (eop == "help"){
+            
+			cout<<"exp2"<<endl;
+			cout<<"exp"<<endl;
+			cout<<"exit"<<endl;
+            
+			;
+		}
+		else if(eop == "exp2"){
+            
+			long in;
+			long out;
+			int i;
+            
+			cout <<"Exponents"<<endl;
+            
+			cout<<"squared: ";
+			cin >> in;
+            
+			i = 1;
+            
+			while(i < 2){
+                
+				i++;
+				out = (in * in);
+                
+			}
+            
+			cout<< in << "^2 = " << out <<endl;
+		}
+        
+		else if(eop == "exp"){
+            
+			long x;
+			long y;
+			long ys;
+			long xs;
+            
+			cout <<"Exponents"<<endl;
+            
+			cin >> x;
+			cout << "power of: ";
+			cin >> y;
+            
+			xs = x;
+			ys = y;
+            
+			while (y > 1){
+                
+				y--;
+				x = (x * xs);
+			}
+            
+			cout << xs << "^" << ys << " = " << x <<endl;
+            
+            
+		}
+		else if(eop == "exit"){
+            
+			return 0;
+		}
+	}
+}
+int pro() {
+
+	long a;
+	long b;
+	long out;
+	       	        	                   
+    cout<< "percentage" <<endl;
+   
+    cout <<"type in the whole: "; //take in the whole
+    cin >> b;
+    cout <<"type in the part: "; //take in the part
+    cin >> a;
+   
+    a = (a * 100); //the part is muiltpluied by 100
+    out = (a / b); //part is / by the whole
+   
+    cout << "%" << out << endl; //the awnser is printed
+    return 0;
+}
+int func() {
+
+	string fop;
+	fop = " ";
+    
+	cout << "-------------------------------------------------" <<endl;
+    
+    
+	while(fop != "exit"){
+        
+		cout << ">:";
+		cin >> fop;
+        
+		if(fop == "y=") {
+           
+			int x1;
+			int x2;
+			int y1;
+			int y2;
+            
+			cout<< "cwardnit 1" <<endl;
+			cout<< "x: ";
+			cin >> x1;
+			cout << "y: ";
+			cin >> y1;
+			cout<< "cwardnit 2" <<endl;
+			cout<< "x: ";
+			cin >> x2;
+			cout << "y: ";
+			cin >> y2;
+            
+			int m;
+			int x;
+			int y;
+            
+			x = (x1 - x2);
+			y = (y1 - y2);
+			m = (y / x);
+            
+			int b;
+            
+			b = (y1 - (m * x1));
+            
+			if (y / x == 0 && y / x < 0){
+                
+				cout << "y = " << m << "x + " << b <<endl;
+			}
+			else {
+                
+				cout <<"y = (" << y << "/" << x << ")x + " << b <<endl; 
+			}
+            
+			continue;
+		}
+		else if(fop == "cor"){
+            
+			int m;
+			int b;
+            
+			cout << "m = ";
+			cin >> m;
+			cout << "b = ";
+			cin >> b;
+            
+			cout << "y = " << m << "x + " << b <<endl;
+            
+			int y1;
+			int y2;
+            
+			y1 = (b + m);
+			y2 = (b - m);
+            
+			cout << "(1, " << y1 << ") (0, " << b << ") (-1, " << y2 << ")" <<endl;
+            
+		}
+		else if(fop == "help"){
+            
+			cout << "y="<<endl;
+			cout << "cor" <<endl;
+            
+			continue;
+		}	
+	}
+return 0;
+}
 int main() {
     
     
 	string op = "";
     
 	cout<<"welcome to SCOS"<<endl; //the introduction
-	cout<<"type help it you need help"<<endl; //prompts the user to type help if they dont know the commands ro runt this prog
+	cout<<"type 'help' it you need help"<<endl; //prompts the user to type help if they dont know the commands ro runt this prog
     
     
 	while(op != "exit") {
-		cout<<">: ";
+		cout<<">:";
 		cin >> op;
 		if (op == "help"){ //when you type "help" you can see all the commands that you can use
             
@@ -34,255 +280,37 @@ int main() {
 			continue;
 		}
 		else if(op == "add"){//addition
-            
-			long x;
-			long y;
-			long sum;
-            
-			cout<<"addition"<<endl;
-            
-			cin >> x;
-			cout <<"+";
-			cin >> y;
-            
-			sum = (x + y);
-            
-			cout << x <<" + "<< y <<" = "<< sum <<endl;
-            
+            add();
 			continue; //not necessary but nice, same for all the other "continues"
 		}
 		else if(op == "sub"){//subtraction
-            
-			long x;
-			long y;
-			long sum;
-            
-			cout<<"subtraction"<<endl;
-            
-			cin >> x;
-			cout <<"-";
-			cin >> y;
-            
-			sum = (x - y);
-            
-			cout << x <<" - "<< y <<" = "<< sum <<endl;
-            
+            subtract();
 			continue;
 		}
 		else if(op == "mul"){//Multiplication
-            
-			long x;
-			long y;
-			long sum;
-            
-			cout<<"Multiplication"<<endl;
-            
-			cin >> x;
-			cout <<"*";
-			cin >> y;
-            
-			sum = (x * y);
-            
-			cout << x <<" * "<< y <<" = "<< sum <<endl;
-            
+           
+            mul(); 
 			continue;
 		}
 		else if(op == "dev"){//division
             
-			long x;
-			long y;
-			long sum;
-            
-			cout<<"division"<<endl;
-            
-			cin >> x;
-			cout <<"/";
-			cin >> y;
-            
-			sum = (x / y);
-            
-			cout << x <<" / "<< y <<" = "<< sum <<endl;
-			continue;
+            dev();
+            continue;
 		}
 		else if(op == "exp"){//expnets
             
-			string eop;  //to make shue that this is set to nothing
-			eop = " ";
-            
-			cout<< "-------------------------------------------------"<<endl;
-            
-			while(eop != "exit"){
-                
-				cout << ">: ";
-				cin >> eop;
-                
-				if (eop == "help"){
-                    
-					cout<<"exp2"<<endl;
-					cout<<"exp"<<endl;
-					cout<<"exit"<<endl;
-                    
-					;
-				}
-				else if(eop == "exp2"){
-                    
-					long in;
-					long out;
-					int i;
-                    
-					cout <<"Exponents"<<endl;
-                    
-					cout<<"squared: ";
-					cin >> in;
-                    
-					i = 1;
-                    
-					while(i < 2){
-                        
-						i++;
-						out = (in * in);
-                        
-					}
-                    
-					cout<< in << "^2 = " << out <<endl;
-				}
-                
-				else if(eop == "exp"){
-                    
-					long x;
-					long y;
-					long ys;
-					long xs;
-                    
-					cout <<"Exponents"<<endl;
-                    
-					cin >> x;
-					cout << "power of: ";
-					cin >> y;
-                    
-					xs = x;
-					ys = y;
-                    
-					while (y > 1){
-                        
-						y--;
-						x = (x * xs);
-					}
-                    
-					cout << xs << "^" << ys << " = " << x <<endl;
-                    
-                    
-				}
-				else if(eop == "exit"){
-                    
-					continue;
-				}
-			}
-		}
+            dev();
+            continue;
+        }	
 		else if (op == "pro"){
             
-            
-	        long a;
-	        long b;
-	        long out;
-            
-	        cout<< "percentage" <<endl;
-            
-	        cout <<"type in the whole: "; //take in the whole
-	        cin >> b;
-	        cout <<"type in the part: "; //take in the part
-	        cin >> a;
-            
-	        a = (a * 100); //the part is muiltpluied by 100
-	        out = (a / b); //part is / by the whole
-            
-	        cout << "%" << out << endl; //the awnser is printed
+        	pro();
 	        continue;
 		}
 		else if(op == "func"){
             
-			string fop;
-			fop = " ";
-            
-			cout << "-------------------------------------------------" <<endl;
-            
-            
-			while(fop != "exit"){
-                
-				cout << ">:";
-				cin >> fop;
-                
-				if(fop == "y=") {
-                    
-					int x1;
-					int x2;
-					int y1;
-					int y2;
-                    
-					cout<< "cwardnit 1" <<endl;
-					cout<< "x: ";
-					cin >> x1;
-					cout << "y: ";
-					cin >> y1;
-					cout<< "cwardnit 2" <<endl;
-					cout<< "x: ";
-					cin >> x2;
-					cout << "y: ";
-					cin >> y2;
-                    
-					int m;
-					int x;
-					int y;
-                    
-					x = (x1 - x2);
-					y = (y1 - y2);
-					m = (y / x);
-                    
-					int b;
-                    
-					b = (y1 - (m * x1));
-                    
-					if (y / x == 0 && y / x < 0){
-                        
-						cout << "y = " << m << "x + " << b <<endl;
-					}
-					else {
-                        
-						cout <<"y = (" << y << "/" << x << ")x + " << b <<endl; 
-					}
-                    
-					continue;
-				}
-				else if(fop == "cor"){
-                    
-					int m;
-					int b;
-                    
-					cout << "m = ";
-					cin >> m;
-					cout << "b = ";
-					cin >> b;
-                    
-					cout << "y = " << m << "x + " << b <<endl;
-                    
-					int y1;
-					int y2;
-                    
-					y1 = (b + m);
-					y2 = (b - m);
-                    
-					cout << "(1, " << y1 << ") (0, " << b << ") (-1, " << y2 << ")" <<endl;
-                    
-				}
-                
-				else if(fop == "help"){
-                    
-					cout << "y="<<endl;
-					cout << "cor" <<endl;
-                    
-					continue;
-				}	
-			}
+			func();
+			continue;
 		}
 	}
     
@@ -291,4 +319,5 @@ int main() {
      */
     
 	return 0;
+	
 }
